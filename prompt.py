437ee0369d681e8dc8ff2e7451469fa67586069b
robson <robson.mantelli@gmail.com>
2025-02-06 23:06:31 -0300

    portatil

--- prompt.py
+++ prompt.py
@@ -1,5 +1,8 @@
 import subprocess
 
+# Defina o caminho fixo para o executável do Windows Terminal portátil
+WT_PATH = r"D:\Portables\WindowsTerminal\wt.exe"  # <-- Defina o caminho correto!
+
 # Defina os diretórios que cada aba do terminal deve abrir e o tipo de shell para cada aba
 abas = [
     {"caminho": "C:/Users/Robso/Documents/Arduino", "shell": "gitbash"},  # Git Bash
@@ -8,17 +11,16 @@ abas = [
     {"caminho": "C:/Users", "shell": "cmd"}  # Prompt de Comando
 ]
 
-# Caminho para o executável do Git Bash
+# Caminho fixo para o executável do Git Bash
 git_bash_path = r'"C:\Program Files\Git\bin\bash.exe" --login -i'
 
 # Monta o comando para abrir o Windows Terminal com abas específicas
-comando = "wt"
+comando = f'"{WT_PATH}"'
 for aba in abas:
     caminho = aba["caminho"]
     shell = aba["shell"]
     
     if shell == "gitbash":
-        # Abrindo o Git Bash chamando diretamente o executável com o diretório correto
         comando += f' new-tab {git_bash_path} -c "cd \\"{caminho}\\" && exec bash" ;'
     elif shell == "powershell":
         comando += f' new-tab powershell -NoExit -Command "Set-Location -Path {caminho}" ;'
@@ -27,3 +29,5 @@ for aba in abas:
 
 # Executa o comando
 subprocess.Popen(comando, shell=True)
+
+print("Windows Terminal aberto com abas específicas de diferentes terminais.")
